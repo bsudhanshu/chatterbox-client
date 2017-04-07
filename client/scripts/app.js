51b851cc001a9d51d95d183adb350df5164ce77f
Hack Reactor Student <student@hackreactor.com>
2017-04-07 12:31:41 -0700

    Updated to pass for send

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,25 @@
-// YOUR CODE HERE:
+var app = {
+  init() {
+    this.send = function(message) {
+      $.ajax({
+        url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+        type: 'POST',
+        data: JSON.stringify(message),
+        contentType: 'application/json',
+        success: function (data) {
+          console.log('chatterbox: Message sent');
+        },
+        error: function (data) {
+          console.error('chatterbox: Failed to send message', data);
+        }
+      })
+    }
+
+    this.fetch = function () {
+      $.ajax({
+        
+      })
+    }
+  }
+};
+
